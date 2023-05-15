52a1d2e4055eedd765fbb2242af2578537e8b5e3
Lev <87573704+Lkupybida@users.noreply.github.com>
2023-05-15 12:59:37 +0300

    Update DeskLamp.py

--- DeskLamp/ua/lviv/iot/part1/lab1/DeskLamp.py
+++ DeskLamp/ua/lviv/iot/part1/lab1/DeskLamp.py
@@ -1,25 +1,49 @@
 class DeskLamp:
-    instance = None
+    """
+    DeskLamp class for first lab
+    """
+    __instance = None
 
-    def __init__(self, is_on=False, brightness=69, color='red', producer='I'):
+    def __init__(self, is_on=False, brightness=69, color='blue', producer='I'):
+        """
+        Initialize the DeskLamp object with default values
+        """
         self.is_on = is_on
         self.brightness = brightness
         self.color = color
         self.producer = producer
 
-    @staticmethod
-    def get_instance():
-        if DeskLamp.instance is None:
-            DeskLamp.instance = DeskLamp()
-        return DeskLamp.instance
+    def __str__(self):
+        """
+        Return a string representation of the DeskLamp object
+        """
+        return f"{self.is_on}, {self.brightness}, {self.color}, {self.producer}"
+
+    @classmethod
+    def get_instance(cls):
+        """
+        Return a string representation of the DeskLamp object
+        """
+        if cls.__instance is None:
+            cls.__instance = DeskLamp()
+        return cls.__instance
 
     def turn_on(self):
+        """
+        Turn on the desk lamp
+        """
         self.is_on = True
 
     def turn_off(self):
+        """
+        Turn off the desk lamp
+        """
         self.is_on = False
 
     def set_brightness(self, value):
+        """
+        Set the brightness of the desk lamp within a valid range
+        """
         if value < 1:
             self.brightness = 1
         elif value > 10:
@@ -28,4 +52,7 @@ class DeskLamp:
             self.brightness = value
 
     def set_color(self, color):
+        """
+        Set the color of the desk lamp
+        """
         self.color = color
