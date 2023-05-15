bf34a5126c2aa2dc3daa647206c601f6d0e6d936
Lev <87573704+Lkupybida@users.noreply.github.com>
2023-05-15 20:24:07 +0300

    Update main.py

--- DeskLamp/ua/lviv/iot/part1/lab1/main.py
+++ DeskLamp/ua/lviv/iot/part1/lab1/main.py
@@ -1,15 +1,39 @@
-from DeskLamp import DeskLamp
+"""
+Create a list of DeskLamp objects with different configurations
+"""
+from desk_lamp import DeskLamp
 
 if __name__ == "__main__":
-    """
-    Create a list of DeskLamp objects with different configurations
-    """
     lamps = [
         DeskLamp(),
         DeskLamp(True, 9, 'red', 'China'),
         DeskLamp.get_instance(),
         DeskLamp.get_instance()
     ]
-
     for i in lamps:
         print(i)
+
+def add_list_of_numbers(args):
+    """
+    Adds up any amount of numbers.
+    """
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+def add_numbers(*args):
+    """
+    Adds up any amount of numbers.
+    """
+    total = 0
+    for num in args:
+        total += num
+    return total
+
+result = add_numbers(500, 700)
+print(result)
+
+numbers = [10, 20]
+result = add_numbers(*numbers)
+print(result)
